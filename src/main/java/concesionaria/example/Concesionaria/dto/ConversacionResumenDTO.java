bfe64872150733e3648166e5b08385cf674ef81c
FedericoMeniy <federicomeniy@gmail.com>
2026-08-21 15:00:18 -0300

    ConversacionController: Se agrego una variable noLeidos para llevar la cuenta de los mensajes no leidos ConversacionResumenDTO: Se agrego la variable cantidadNoLeidos MensajeRepository: Se agrego el metodo para llevar la cuenta de los no leidos

--- src/main/java/concesionaria/example/Concesionaria/dto/ConversacionResumenDTO.java
+++ src/main/java/concesionaria/example/Concesionaria/dto/ConversacionResumenDTO.java
@@ -8,12 +8,13 @@ public class ConversacionResumenDTO {
     private Long conversacionId;
     private Long publicacionId;
 
-    // Acá guardamos el email de la OTRA persona (para saber con quién hablo)
+    // Aca guardamos el email de la OTRA persona (para saber con quien hablo)
     private String emailContacto;
 
-    // Para que Angular sepa si en este chat soy el dueño del auto o el interesado
+    // Para que Angular sepa si en este chat soy el duenio del auto o el interesado
     private String rol; // "COMPRADOR" o "VENDEDOR"
     private String nombreContacto;
     private String ultimoMensaje;
     private LocalDateTime fechaUltimoMensaje;
+    private int cantidadNoLeidos;
 }
